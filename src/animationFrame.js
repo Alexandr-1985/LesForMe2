1bc2ea32648b2e3cf8e512be3b76e7cd9810ff48
Alexandr <alexandrgorr@gmail.com>
2021-12-10 13:11:47 +0200

    change parametrs of airplane

--- src/animationFrame.js
+++ src/animationFrame.js
@@ -6,15 +6,6 @@ const man = document.querySelector(".man");
 let active = false;
 let count = 0;
 let idInterval;
-let clientX = 0;
-let clientY = 0;
-
-//document.elementFromPoint(clientX, clientY).createElement({
-man.element.style = {
-    top: 152 + "px",
-    left: 0 + "px",
-};
-//});
 
 const availableScreenWidth = window.innerWidth - 520;
 const availableScreenHeight = window.innerHeight - 220;
@@ -24,21 +15,23 @@ const flyAnimate = () => {
     console.log(count);
 
     man.style.position = "absolute";
+    //man.style.left = 0 + "px";
+    // man.style.top = 152 + "px";
     airplane.style.position = "absolute";
 
     idInterval = requestAnimationFrame(flyAnimate);
 
-    if (count < availableScreenWidth && count < availableScreenHeight) {
+    if (count < availableScreenHeight) {
         man.style.top = count + "px";
-        airplane.style.left = count * 2 + "px";
-    } else if (count < availableScreenHeight) {
-        airplane.style.left = count * 2 + "px";
+        airplane.style.left = count + "px";
+    } else if (count < availableScreenWidth) {
+        airplane.style.left = count + "px";
     } else {
         cancelAnimationFrame(idInterval);
     }
 };
 
-//let timerId = setInterval(flyAnimate, 1000);
+let timerId = setInterval(flyAnimate, 1000);
 
 document.addEventListener("click", () => {
     if (active) {
